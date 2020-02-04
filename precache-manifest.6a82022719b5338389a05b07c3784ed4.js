5ca8456739225e63d3a488d10b91260381a21614
nishekh <nishekhgowda143@gmail.com>
2020-02-04 22:21:01 +0530

    Updates

---
+++ precache-manifest.6a82022719b5338389a05b07c3784ed4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c795ad35b3092f8c49bdcb3fa5ebf4d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b7d04393556469edbdb9",
+    "url": "/monster-rolodex/static/css/main.79201425.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d04393556469edbdb9",
+    "url": "/monster-rolodex/static/js/main.a34d54f1.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
